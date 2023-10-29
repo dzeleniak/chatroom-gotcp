7383c40b9209465cb6a7e72da4071b41f35cc52d
dzeleniak <zeleniak.david@gmail.com>
2023-10-29 12:20:14 -0700

    reconfigure to use internal

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/dzeleniak/chatroom-gotcp
 
 go 1.21.0
 
-require github.com/gofrs/uuid v4.4.0+incompatible // indirect
+require github.com/gofrs/uuid v4.4.0+incompatible
--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"github.com/dzeleniak/chatroom-gotcp/server"
+	"github.com/dzeleniak/chatroom-gotcp/internal/server"
 )
 
 func main() {
 	s, _ := server.New()
-
+	
 	s.Listen(8080)
 }
