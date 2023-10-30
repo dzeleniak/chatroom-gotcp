b6dc814b89f4fd43b7bffd33ac8c87727f7ba8ef
dzeleniak <zeleniak.david@gmail.com>
2023-10-29 20:27:01 -0700

    Add base of chat TUI
    Using Charm packages

---
+++ cmd/client/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/charmbracelet/bubbles/textarea"
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+func main() {
+	p := tea.NewProgram(initialModel())
+
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type errMsg error;
+
+type model struct {
+	viewport viewport.Model
+	messages []string
+	textarea textarea.Model
+	senderStyle lipgloss.Style
+	err error
+}
+
+func initialModel() model {
+	ta := textarea.New()
+	ta.Placeholder = "Send a message..."
+	ta.Focus();
+
+	ta.Prompt = "| "
+	ta.CharLimit = 200
+
+	ta.SetWidth(30)
+	ta.SetHeight(3);
+	ta.FocusedStyle.CursorLine = lipgloss.NewStyle()
+	ta.ShowLineNumbers = false;
+
+	vp := viewport.New(100, 5)
+	vp.SetContent(`Welcome! Type a message and press Enter to send.`)
+
+	ta.KeyMap.InsertNewline.SetEnabled(false)
+
+	return model{
+		textarea: ta,
+		viewport: vp,
+		messages: []string{},
+		senderStyle: lipgloss.NewStyle().Foreground(lipgloss.Color("5")),
+		err: nil,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return textarea.Blink
+}
+
+func (m model) View() string {
+	return fmt.Sprintf(
+		"%s\n\n%s",
+		m.viewport.View(),
+		m.textarea.View(),
+	) + "\n\n"
+}
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var (
+		tiCmd tea.Cmd
+		vpCmd tea.Cmd
+	)
+
+	m.textarea, tiCmd = m.textarea.Update(msg)
+	m.viewport, vpCmd = m.viewport.Update(msg)
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg: 
+		switch msg.Type {
+			case tea.KeyCtrlC, tea.KeyEsc: 
+				fmt.Println(m.textarea.Value())
+				return m, tea.Quit
+			case tea.KeyEnter:
+				m.messages = append(m.messages, m.senderStyle.Render("You: ") + m.textarea.Value())
+				m.viewport.SetContent(strings.Join(m.messages, "\n"))
+				m.textarea.Reset()
+				m.viewport.GotoBottom()
+		}
+	case errMsg:
+		m.err = msg
+		return m, nil
+	}
+
+	return m, tea.Batch(tiCmd, vpCmd);
+}
